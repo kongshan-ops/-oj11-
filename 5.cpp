ef08e17f9876ad9f88b4caca80f408ced807b84e
kongshan-ops <141298045+kongshan-ops@users.noreply.github.com>
2025-07-07 20:27:01 +0800

    Add files via upload
    完成了第4、5、6题

---
+++ 5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+//using namespace std;
+//int main() {
+//	int a;
+//	while (scanf_s("%d", &a) != EOF) {
+//		if (a >= 90 && a <= 100) {
+//			printf("A\n");
+//		}
+//		else if (a >= 80 && a <= 89) {
+//			printf("B\n");
+//		}
+//		else if (a >= 70 && a <= 79) {
+//			printf("C\n");
+//		}
+//		else if (a >= 60 && a <= 69) {
+//			printf("D\n");
+//		}
+//		else if (a >= 0 && a <= 59) {
+//			printf("E\n");
+//		}
+//		else {
+//			printf("Score is error!\n");
+//		}
+//			
+//	}
+//}
